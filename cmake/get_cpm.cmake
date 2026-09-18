7fe85d0efbfd37a2aeaa0e8ec81f4dc078a15c4e
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2026-09-18 20:43:22 +0200

    Verify download

--- cmake/get_cpm.cmake
+++ cmake/get_cpm.cmake
@@ -1,4 +1,5 @@
 set(CPM_DOWNLOAD_VERSION 0.43.1)
+set(CPM_HASH_SUM "1c40fc102ce9625d7de7eb14f541cab30cc3138dca627f0b0ec40293ce6c2934")
 
 if(CPM_SOURCE_CACHE)
   set(CPM_DOWNLOAD_LOCATION "${CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
@@ -15,6 +16,8 @@ function(download_cpm)
   file(DOWNLOAD
        https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
        ${CPM_DOWNLOAD_LOCATION}
+       TLS_VERIFY ON
+       EXPECTED_HASH SHA256=${CPM_HASH_SUM}
   )
 endfunction()
 
